9abea60e4cdd72c43d163a016cc2d3fd2487a413
Cristian Richardo Anin <richardoanin@student.telkomuniversity.ac.id>
2022-04-13 22:04:18 +0700

    Masuk Laravel

---
+++ Mindy Laravel/resources/views/landing.blade.php
@@ -0,0 +1,162 @@
+@extends('layout')
+@section('content')
+
+<section id="hero">
+        <div class="container my-5 py-5">
+            <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-5">
+                    <h1>Kesehatan adalah hal yang penting</h1>
+                    <p class="mt-3">Menjaga kesehatan mental anda merupakan hal yang harus menjadi sebuah
+                        prioritas bagi diri anda.</p>
+                    <a href="" class="mt-2 btn btn-success py-2 px-5 rounded-pill">Start</a>
+                </div>
+                <div class="col-5">
+                    <div class="position-absolute bg-success w-100 h-75 rounded-circle" style="opacity: 0.25;"></div>
+                    <img src="/Landing Page/Person.svg" alt="" class="position-relative">
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+
+    <section id="2">
+        <div class="container my-5 py-5">
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <h2 class="text-center">Kenapa Memilih Kami</h2>
+                    <div class="row mt-5">
+                        <div class="col">
+                            <div class="row d-flex align-items-center justify-content-center">
+                                <div class="bg-light p-3 rounded-circle mr-3">
+                                    <img src="/Landing Page/$.svg" alt="">
+                                </div>
+                                <h4>Harga Terjangkau</h4>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row d-flex align-items-center justify-content-center">
+                                <div class="bg-light p-3 rounded-circle mr-3">
+                                    <img src="/Landing Page/v.svg" alt="">
+                                </div>
+                                <h4>Tersertifikasi</h4>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="row d-flex align-items-center justify-content-center">
+                                <div class="bg-light     p-3 rounded-circle mr-3">
+                                    <img src="/Landing Page/pin.svg" alt="">
+                                </div>
+                                <h4>Dimana Saja</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="3">
+        <div class="container my-5 py-5">
+            <div class="row justify-content-center">
+                <div class="col-12 bg-light text-center p-5 rounded-lg">
+                    <h2>Pricelist</h2>
+                    <p class="mb-5">Menawarkan harga yang menarik</p>
+                    <div class="row d-flex mt-5">
+                        <div class="col border text-left p-4 rounded-lg m-2">
+                            <h5>Prod 1</h5>
+                            <h2 class="mb-4">Rp. 25.000</h2>
+                            <div class="row d-flex px-3 mb-5">
+                                <img src="/Landing Page/v.svg" alt="" class="mr-2" style="height: 20px;">
+                                <p>Pertemuan 1 X 1 Minggu</p>
+                            </div>
+                            <a href="" class="btn btn-dark w-100 mt-5">Start</a>
+                        </div>
+                        <div class="col border text-left p-4 rounded-lg m-2">
+                            <h5>Prod 2</h5>
+                            <h2 class="mb-4">Rp. 50.000</h2>
+                            <div class="row d-flex px-3">
+                                <img src="/Landing Page/v.svg" alt="" class="mr-2" style="height: 20px;">
+                                <p>Pertemuan 3 X 1 Minggu</p>
+                            </div>
+                            <div class="row d-flex px-3">
+                                <img src="/Landing Page/v.svg" alt="" class="mr-2" style="height: 20px;">
+                                <p>Konsultasi via chat 2 X 1 Minggu</p>
+                            </div>
+                            <a href="" class="btn btn-dark w-100 mt-5">Start</a>
+                        </div>
+                        <div class="col border text-left p-4 rounded-lg m-2">
+                            <h5>Prod 3</h5>
+                            <h2 class="mb-4">Rp. 100.000</h2>
+                            <div class="row d-flex px-3">
+                                <img src="/Landing Page/v.svg" alt="" class="mr-2" style="height: 20px;">
+                                <p>Pertemuan 5 X 1 Minggu</p>
+                            </div>
+                            <div class="row d-flex px-3">
+                                <img src="/Landing Page/v.svg" alt="" class="mr-2" style="height: 20px;">
+                                <p>Konsultasi via chat 3 X 1 Minggu</p>
+                            </div>
+                            <a href="" class="btn btn-dark w-100 mt-5">Start</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+
+    <section id="4">
+        <div class="container my-5 py-5">
+            <div class="row d-flex justify-content-between align-items-center">
+                <div class="col-5">
+                    <div class="position-absolute bg-success w-100 h-75 rounded-circle" style="opacity: 0.25;"></div>
+                    <img src="/Landing Page/A Person.svg" class="position-relative" alt="">
+                </div>
+                <div class="col-5">
+                    <h2>Tenangkan Dirimu dan Berlatih Untuk Menerima Diri</h2>
+                    <p class="mt-3">Menjaga kesehatan mental anda merupakan hal yang harus menjadi sebuah
+                        prioritas bagi diri anda.</p>
+                    <a href="" class="mt-2 btn btn-success py-2 px-5 rounded-pill">Start</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="5">
+        <div class="container my-5">
+            <div class="row d-flex bg-success p-5 rounded-lg align-items-center">
+                <div class="col-10">
+                    <h1 class="text-light">Mulai Course Di Mindy,
+                        Gratis Yuk
+                    </h1>
+                    <p class="text-light">
+                        Kamu bisa mulai mengikuti course di Mindy Secara Gratis dan mendapatkan banyak kejutan lainnya
+                    </p>
+                </div>
+                <div class="col-2">
+                    <a href="" class="mt-2 btn btn-light py-2 px-5 rounded-pill">Start</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+
+    <section class="6">
+        <div class="container my-5 py-5">
+            <h2 class="text-center">Kantor Kami</h2>
+            <div class="row d-flex justify-content-between mt-5">
+                <div class="col-8">
+                    <iframe class="w-100"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.573603666437!2d106.88217375064512!3d-6.187771595498368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4e918384361%3A0xe1f0f577574583d8!2sJl.%20Locis%20No.10%2C%20RT.10%2FRW.17%2C%20Kayu%20Putih%2C%20Kec.%20Pulo%20Gadung%2C%20Kota%20Jakarta%20Timur%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2013210!5e0!3m2!1sen!2sid!4v1649621075592!5m2!1sen!2sid"
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-4">
+                    <p class="font-weight-bold m-0">Alamat</p>
+                    <p class="mb-4">Jl.Locis No. 10 Kayu Putih, Jakata Timur</p>
+                    <p class="font-weight-bold m-0">E-mail</p>
+                    <p>hellomindy@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
