89005e800a0c86bc1c7962d1ad860c3f585fbb8a
Cristian Richardo Anin <richardoanin@student.telkomuniversity.ac.id>
2022-04-15 23:27:56 +0700

    Add: Backend Profile User ref #6

---
+++ Mindy Laravel/database/migrations/2022_04_15_135137_create_table_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('nomor');
+            $table->string('lahir');
+            $table->string('kelamin');
+            $table->string('pass');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_user');
+    }
+}
