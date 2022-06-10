2317158d64cd43f08ea1cbb263a99b1e3bb86a60
Cristian Richardo Anin <richardoanin@student.telkomuniversity.ac.id>
2022-06-10 08:31:03 +0700

    backend konfirmasi status pembayaran ref #10

--- Mindy Laravel/app/Http/Controllers/Admin/PaymentController.php
+++ Mindy Laravel/app/Http/Controllers/Admin/PaymentController.php
@@ -13,4 +13,12 @@ class PaymentController extends Controller
         $pembelian = Pembelian::join('users', 'pembelians.id_user', '=', 'users.id')->join('products', 'pembelians.pilihan', '=', 'products.paket')->get(['pembelians.*', 'users.nama', 'products.paket', 'products.harga']);
         return view('admin.AdminPayment', compact('pembelian'));
     }
+
+    public function update(Request $request, $id)
+    {
+        Pembelian::find($id)->update([
+            'status' => $request->status
+        ]);
+        return redirect("/payment");
+    }
 }
